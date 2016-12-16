1b9cb4a01d1108c97113cec8e6a669d75e15615a
John Pham <dpha010@aucklanduni.ac.nz>
2016-12-16 21:17:57 +1300

    Quick session: Stack clone

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class MainMenu : MonoBehaviour {
+
+
+	public Text score;
+	private void Start ()
+	{
+
+		score.text = PlayerPrefs.GetInt ("score").ToString();
+
+
+	}
+
+
+	public void ToGame(){
+		SceneManager.LoadScene("Main");
+	}
+}
